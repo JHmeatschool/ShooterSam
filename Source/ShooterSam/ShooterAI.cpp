7f58b5bef3dd3b56b20202862b2f346a0b8a5713
meatschool <ceojr1234@gmail.com>
2026-02-24 21:34:38 +0900

    feat: Add BTService_PlayerLocationIfSeen with TickNode logging

--- Source/ShooterSam/ShooterAI.cpp
+++ Source/ShooterSam/ShooterAI.cpp
@@ -50,7 +50,6 @@ void AShooterAI::StartBehaviorTree(AShooterSamCharacter* Player)
 		UBlackboardComponent* MyBlackboard = GetBlackboardComponent();
 		if (MyBlackboard && PlayerCharacter && MyCharacter)
 		{
-			//MyBlackboard->SetValueAsVector(TEXT("PlayerLocation"), PlayerCharacter->GetActorLocation());
 			MyBlackboard->SetValueAsVector(TEXT("StartLocation"), MyCharacter->GetActorLocation());
 		}
 	}
