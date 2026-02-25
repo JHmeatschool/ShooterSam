f0cbe6efb1dc67ac3a3a6fd25e6083c4b5363851
meatschool <ceojr1234@gmail.com>
2026-02-25 20:29:50 +0900

    feat: Add BTTaskNode_Shoot and implement conditional shooting loop in Behavior Tree

--- Source/ShooterSam/ShooterSamCharacter.cpp
+++ Source/ShooterSam/ShooterSamCharacter.cpp
@@ -169,6 +169,7 @@ void AShooterSamCharacter::OnDamageTaken(AActor* DamagedActor, float Damage, con
 			IsAlive = false;
 			Health = 0.0f;
 			GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+			DetachFromControllerPendingDestroy();
 
 			UE_LOG(LogTemp, Display, TEXT("Character died: %s"), *GetActorNameOrLabel());
 		}
