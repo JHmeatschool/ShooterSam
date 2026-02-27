c649fbe596c6d481bed3a5fec7769c53dcffce5b
meatschool <ceojr1234@gmail.com>
2026-02-27 20:10:40 +0900

    feat: Add randomized sound cues for both shooting and bullet impacts

--- Source/ShooterSam/Gun.cpp
+++ Source/ShooterSam/Gun.cpp
@@ -38,6 +38,7 @@ void AGun::Tick(float DeltaTime)
 void AGun::PullTrigger()
 {
 	MuzzleFlashParticleSystem->Activate(true);
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), ShootSound, GetActorLocation());
 
 	if (OwnerController)
 	{
@@ -55,6 +56,7 @@ void AGun::PullTrigger()
 		if (IsHit)
 		{
 			UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactParticleSystem, HitResult.ImpactPoint, HitResult.ImpactPoint.Rotation());
+			UGameplayStatics::PlaySoundAtLocation(GetWorld(), ImpactSound, HitResult.ImpactPoint);
 
 			AActor* HitActor = HitResult.GetActor();
 			if (HitActor)
